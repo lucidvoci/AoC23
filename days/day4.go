8fa617b5bd4988ea7338ab4f5def65f580c30263
Lucie Charvat <luciecharvat@microsoft.com>
2023-12-04 10:58:36 +0100

    day 4

--- days/day4.go
+++ days/day4.go
@@ -43,7 +43,6 @@ func Day4() {
 	}
 }
 
-// Card   1: 33 56 23 64 92 86 94  7 59 13 | 86 92 64 43 10 70 16 55 79 33 56  8  7 25 82 14 31 96 94 13 99 29 69 75 23
 func copies(line string, gameNum int, res *[206]int) {
 	win := make(map[string]bool)
 	game := strings.Split(line, ":")
@@ -67,32 +66,3 @@ func copies(line string, gameNum int, res *[206]int) {
 		res[i] += res[gameNum]
 	}
 }
-
-// func getPoints(line string) int {
-// 	var sum int
-// 	win := make(map[string]bool)
-// 	game := strings.Split(line, ":")
-// 	nums := strings.Split(game[1], "|")
-// 	for _, winNum := range strings.Split(nums[0], " ") {
-// 		cleanNum := strings.TrimSpace(winNum)
-// 		_, err := strconv.Atoi(cleanNum)
-// 		if err != nil {
-// 			continue
-// 		}
-// 		win[cleanNum] = true
-// 	}
-// 	for _, myNum := range strings.Split(nums[1], " ") {
-// 		cleanNum := strings.TrimSpace(myNum)
-// 		if win[cleanNum] && sum == 0 {
-// 			fmt.Println("-" + cleanNum)
-
-// 			sum = 1
-// 		} else if win[cleanNum] {
-// 			fmt.Println("--" + cleanNum)
-// 			sum = sum * 2
-// 		}
-// 	}
-
-// 	fmt.Println(sum)
-// 	return sum
-// }
